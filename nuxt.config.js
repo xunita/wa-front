8486e9e4fbdac17840d869555c58d52d52c06585
sopegue <yayasopeguesoro@gmail.com>
2020-10-15 18:20:37 +0100

    update backend api url

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,7 +85,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://waback.herokuapp.com/api',
     credentials: true,
   },
   auth: {
